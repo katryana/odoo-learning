e51d0ffc66c17fcd9385e84f0f8f9a7d7a3a2fa2
katryana <kateryna.zadorozhna.dev@gmail.com>
2024-01-11 11:25:12 +0200

    [ADD] estate: create new model estate_property_type

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -33,3 +33,4 @@ class EstateProperty(models.Model):
         ("sold", "Sold"),
         ("canceled", "Canceled"),
     ], required=True, default="new", copy=False)
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
